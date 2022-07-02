7199e93b8ab8dab4e4aa1f900b4d5d17d3e53e61
Dmitry Klyushkov <charlesfxa@gmail.com>
2022-07-02 20:11:53 +0300

    First commit

---
+++ pages/users/[id].tsx
@@ -0,0 +1,48 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { NextPage } from "next/types";
+import { useState, useEffect } from "react";
+
+interface IUser {
+  id: number;
+  name: string;
+  email: string;
+}
+
+const Details: NextPage = () => {
+  const router = useRouter();
+  const { id } = router.query;
+
+  const [user, setUser] = useState<IUser | null>(null);
+
+  useEffect(() => {
+    async function getUser() {
+      const res = await fetch(
+        `https://jsonplaceholder.typicode.com/users/${id}`
+      );
+      const user: IUser = await res.json();
+
+      setUser(user);
+    }
+
+    if (id) {
+      getUser();
+    }
+  }, [id]);
+
+  return (
+    <>
+      <Link href="/">
+        <a>Home page</a>
+      </Link>
+      {user && (
+        <div>
+          <h3>{user?.name}</h3>
+          <h5>{user?.email}</h5>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Details;
