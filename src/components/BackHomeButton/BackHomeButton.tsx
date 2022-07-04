da0c9fd33d1e2c98051dfb815f885a58a6676bb6
Dmitry Klyushkov <charlesfxa@gmail.com>
2022-07-04 17:00:36 +0300

    sass

--- src/components/BackHomeButton/BackHomeButton.tsx
+++ src/components/BackHomeButton/BackHomeButton.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 
-import styles from "./BackHomeButton.module.css";
+import styles from "./BackHomeButton.module.scss";
 
 const BackHomeButton = () => {
   return (
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import type { InferGetStaticPropsType } from "next";
 import Link from "next/link";
 
-import styles from "../styles/Home.module.css";
+import styles from "../styles/Home.module.scss";
 
 const Home = ({ users }: InferGetStaticPropsType<typeof getStaticProps>) => {
   return (
